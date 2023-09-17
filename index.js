537d67a62f1ce25b5b0b3680aa2834c1f7387d1d
Abhay <kabhay849@gmail.com>
2023-09-18 01:21:26 +0530

    image made

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+const sharp = require('sharp');
+const fs = require('fs')
+require('dotenv').config()
+app.use(cors());
 app.get('/', (req, res) => {
-    res.send('I m LIVE')
+    res.send('I m Live')
 })
-app.post('/hit', (req, res) => {
-    res.send('hello world')
-})
+app.post('/upload', upload.array('files', 12), function (req, res) {
+    const files_array = req.files[0]
+    console.log(files_array)
+    res.send('file Uploaded')
+    sharp(files_array.path)
+        .toFile('./Image/' + files_array.originalname, (err, info) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(info);
+                fs.unlinkSync(files_array.path);
+            }
+        });
+});
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server running")
+})
