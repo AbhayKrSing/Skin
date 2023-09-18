6d8bb745aecdf41eb1bd8607f0248a21117ed319
Abhay <kabhay849@gmail.com>
2023-09-18 20:10:14 +0530

    completed as of now

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 var cors = require('cors');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' })
+const { spawn } = require('child_process')
 const sharp = require('sharp');
 const fs = require('fs')
 require('dotenv').config()
 app.use(cors());
+const path = require('path');
 app.get('/', (req, res) => {
     res.send('I m Live')
 })
@@ -21,6 +23,22 @@ app.post('/upload', upload.array('files', 12), function (req, res) {
                 // console.log(info);  //info of writed file like its format,size etc.
                 console.log('./Image/' + files_array.originalname); //use karna hai model mey
                 fs.unlinkSync(files_array.path);
+                const py = spawn('python', ['script.py', './Image/' + files_array.originalname])
+                py.stdout.on('data', (data) => {
+                    console.log(data.toString())
+                })
+                py.on('close', (close) => {
+                    console.log('Model close at', close);
+                    fs.readdir('./Image', (err, files) => {
+                        if (err) throw err;
+
+                        for (const file of files) {
+                            fs.unlink(path.join('./Image', file), (err) => {
+                                if (err) throw err;
+                            });
+                        }
+                    });
+                })
                 res.send({
                     success: true,
                     message: 'Image File Uploaded'
---
+++ script.py
@@ -0,0 +1,52 @@
+
+def main():
+    import tensorflow as tf
+    from PIL import Image
+    # from tensorflow.keras.preprocessing import image
+    # from tensorflow.keras import Model
+    import cv2
+    import os
+    import numpy as np
+    # import urllib.request
+    from urllib.parse import quote
+    # import requests
+    # import io
+    from urllib.parse import unquote
+    import sys;
+    
+    label = ['Seborrheic Keratoses and other Benign Tumors',
+    'Psoriasis pictures Lichen Planus and related diseases',
+    'Tinea Ringworm Candidiasis and other Fungal Infections',
+    'Eczema Photos',
+    'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions']
+
+    vgg_model = tf.keras.applications.vgg19.VGG19(weights = 'imagenet',  include_top = False, input_shape = (180, 180, 3))
+
+    def predict_skin_disease(dataImage):
+        print('I m model')
+    # Define list of class names
+        class_names = label
+
+        # Load saved model
+        model = tf.keras.models.load_model('skin.h5')
+
+        img = cv2.imread(dataImage)
+        img = cv2.resize(img, (180, 180))
+        img = np.array(img)
+        img = np.expand_dims(img, axis=0)
+        img = vgg_model.predict(img)
+        img = img.reshape(1, -1)
+
+        # Make prediction on preprocessed image
+        pred = model.predict(img)[0]
+        # print(pred)
+        predicted_class_index = np.argmax(pred)
+        # print(predicted_class_index)
+        predicted_class_name = class_names[predicted_class_index]
+
+        return predicted_class_name
+
+    print(predict_skin_disease(sys.argv[1]))
+
+if(__name__=='__main__'):
+    main()
